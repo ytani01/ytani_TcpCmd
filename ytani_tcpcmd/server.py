54b388f5c87f88dccad6b5c7b065cca1d9e28291
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-09-10 00:44:55 +0900

    prepare

--- ytani_tcpcmd/server.py
+++ ytani_tcpcmd/server.py
@@ -1,6 +1,12 @@
 #
 # Copyright (c) 2021 Yoichi Tanibayashi
 #
+import socketserver
+import socket
+import threading
+import queue
+import json
+import time
 import click
 from .my_logger import get_logger
 
@@ -9,6 +15,8 @@ class Server:
     """ Server """
 
     __log = get_logger(__name__, False)
+
+    DEF_PORT = 12345
 
     def __init__(self, debug=False):
         self._dbg = debug
@@ -21,17 +29,42 @@ class Server:
         print('Hello, %s' % (arg1))
 
 
-@click.command(help="Server")
-@click.argument('arg1', type=str)
+class ServerApp:
+    """ ServerApp """
+
+    __log = get_logger(__name__, False)
+
+    def __init__(self, port, debug=False):
+        """ init """
+        self._dbg = debug
+        __class__.__log = get_logger(__class__.__name__, self._dbg)
+        self.__log.debug('port=%s', port)
+
+        self._port = port
+
+    def main(self):
+        """ main """
+        self.__log.debug('')
+
+    def end(self):
+        """ end """
+        self.__log.debug('')
+
+
+@click.command(help="TCP Command Server")
+@click.option('--port', '-p', 'port', type=int, default=Server.DEF_PORT,
+              htlp='port number (default: %s)' % (Server.DEF_PORT))
 @click.option('--debug', '-d', 'debug', is_flag=True, default=False,
               help='debug flag')
 @click.pass_obj
-def server(obj, arg1, debug):
+def server(obj, port, debug):
     """ Server """
     debug = obj['debug'] or debug
-
     __log = get_logger(__name__, debug)
-    __log.debug('obj=%s, arg1=%s', obj, arg1)
+    __log.debug('obj=%s, port=%s', obj, port)
 
-    cmd_obj = Server(debug=debug)
-    cmd_obj.server(arg1)
+    obj = ServerApp(port, debug=debug)
+    try:
+        obj.main()
+    finally:
+        obj.end()
